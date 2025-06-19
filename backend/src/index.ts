5711ce6bf1b569deb1ff56a7f1a49f78e14a1842
Douglas Oliveira <douglasgama0@gmail.com>
2025-06-19 20:57:58 -0300

    fix: alterando porta padrao

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import { connectDB } from './config/database';
 import iaRouter from './routes/ia';
 import path from 'path';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
